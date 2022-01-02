da1a5ac653ed199a0e4711f09c0a0df360f7bf60
Nicolas Combecave <zezebubulonlon@gmail.com>
2022-01-02 13:01:26 +0100

    Rearranged templateDay a little bit

--- 2021/day00/aoc2021_00.py
+++ 2021/day00/aoc2021_00.py
@@ -6,6 +6,57 @@ import glob
 # import re
 import time
 
+EXPECTED_TEST1  = None
+EXPECTED_FINAL1 = None
+EXPECTED_TEST2  = None
+EXPECTED_FINAL2 = None
+
+
+def gogo(verbose=False, part1Test=False, part1Final=False, part2Test=False, part2Final=False):
+    location     = os.path.dirname(__file__)
+    current      = os.path.basename(__file__).split('.')[0]
+    part1_inputs = glob.glob('%s/%s_part1_data.txt' %(location, current))
+    part1_tests  = glob.glob('%s/%s_part1_sample*.txt' %(location, current))
+    part2_inputs = glob.glob('%s/%s_part2_data.txt' %(location, current))
+    part2_tests  = glob.glob('%s/%s_part2_sample*.txt' %(location, current))
+
+    if part1Test:
+        print 'PART 1 TESTS ------------------------------------'
+        for inputFile in part1_tests:
+            print inputFile
+            inputData = getData(inputFile)
+            result = part1(inputData)
+            print 'result =', result, result == EXPECTED_TEST1
+            print
+
+    if part1Final:
+        print 'PART 1 FINAL ------------------------------------'
+        for inputFile in part1_inputs:
+            print inputFile
+            inputData = getData(inputFile)
+            result = part1(inputData)
+            print 'result =', result, result == EXPECTED_FINAL1
+            print
+
+
+    if part2Test:
+        print 'PART 2 TESTS ------------------------------------'
+        for inputFile in part2_tests:
+            print inputFile
+            inputData = getData(inputFile)
+            result = part2(inputData, verbose=verbose)
+            print 'result =', result, result == EXPECTED_TEST2
+            print
+
+    if part2Final:
+        print 'PART 2 FINAL ------------------------------------'
+        for inputFile in part2_inputs:
+            print inputFile
+            inputData = getData(inputFile)
+            result = part2(inputData, verbose=verbose)  # Ma version
+            print 'result =', result, result == EXPECTED_FINAL2
+            print
+
 def timeIt(func):
     def func_decorated(*args, **kwargs):
         startTime = time.time()
@@ -40,56 +91,6 @@ def part2(inputData, verbose=False):
     return result
 
 
-def gogo(verbose=False, part1Test=False, part1Final=False, part2Test=False, part2Final=False):
-    location     = os.path.dirname(__file__)
-    current      = os.path.basename(__file__).split('.')[0]
-    part1_inputs = glob.glob('%s/%s_part1_data.txt' %(location, current))
-    part1_tests  = glob.glob('%s/%s_part1_sample*.txt' %(location, current))
-    part2_inputs = glob.glob('%s/%s_part2_data.txt' %(location, current))
-    part2_tests  = glob.glob('%s/%s_part2_sample*.txt' %(location, current))
-
-    if part1Test:
-        print 'PART 1 TESTS ------------------------------------'
-        expectedResult = None
-        for inputFile in part1_tests:
-            print inputFile
-            inputData = getData(inputFile)
-            result = part1(inputData)
-            print 'result =', result, result == expectedResult
-            print
-
-    if part1Final:
-        expectedResult = None
-        print 'PART 1 FINAL ------------------------------------'
-        for inputFile in part1_inputs:
-            print inputFile
-            inputData = getData(inputFile)
-            result = part1(inputData)
-            print 'result =', result, result == expectedResult
-            print
-
-
-    if part2Test:
-        expectedResult = None
-        print 'PART 2 TESTS ------------------------------------'
-        for inputFile in part2_tests:
-            print inputFile
-            inputData = getData(inputFile)
-            result = part2(inputData, verbose=verbose)
-            print 'result =', result, result == expectedResult
-            print
-
-    if part2Final:
-        print 'PART 2 FINAL ------------------------------------'
-        expectedResult = None
-        for inputFile in part2_inputs:
-            print inputFile
-            inputData = getData(inputFile)
-            result = part2(inputData, verbose=verbose)  # Ma version
-            print 'result =', result, result == expectedResult
-            print
-
-
 if __name__=="__main__":
     print(sys.version)
-    gogo(verbose=False, part1Test=True, part1Final=True, part2Test=True, part2Final=True)
+    gogo(verbose=False, part1Test=True, part1Final=False, part2Test=False, part2Final=False)
